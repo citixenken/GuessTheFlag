45625f8676fb46b4f6c49f556f550fa9b2f29abd
citixenken <kmuyesu@gmail.com>
2021-12-07 21:35:33 +0300

    first commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ken Muyesu on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
