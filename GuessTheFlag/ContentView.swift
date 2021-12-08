8c441f6dfe564dec38ff57abac7aae7f7f9c3c26
citixenken <kmuyesu@gmail.com>
2021-12-08 10:09:35 +0300

    stacks to arrange views

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            HStack {
+                Text("7")
+                    .padding()
+                Text("8")
+                    .padding()
+                Text("9")
+                    .padding()
+            }
+            HStack {
+                Text("4")
+                    .padding()
+                Text("5")
+                    .padding()
+                Text("6")
+                    .padding()
+            }
+            HStack {
+                Text("1")
+                    .padding()
+                Text("2")
+                    .padding()
+                Text("3")
+                    .padding()
+            }
+            HStack {
+                Text("0")
+                    .padding()
+                Text(".")
+                    .padding()
+                Text("Enter")
+                    .padding()
+            }
+            Spacer()
+        }
     }
 }
 
