9d1601cb5a689c3e3a09cd3578bbbfcfc5c82466
citixenken <kmuyesu@gmail.com>
2021-12-08 22:47:15 +0300

    buttons and images

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,45 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    func executeData() {
+        print("Here we go...")
+    }
+    
     var body: some View {
+        
         ZStack {
             //entire screen background
             VStack (spacing: 0){
                 //Color.red
                 //Color.blue
                 
-                
                 //gradients
                 
-//                AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+                //                AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
                 
-//                RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+                //                RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
                 
-    //            LinearGradient(gradient: Gradient(stops: [
-    //                    .init(color: .white, location: 0.30),
-    //                    .init(color: .black, location: 0.70),
-    //            ]), startPoint: .top, endPoint: .bottom)
+                //            LinearGradient(gradient: Gradient(stops: [
+                //                    .init(color: .white, location: 0.30),
+                //                    .init(color: .black, location: 0.70),
+                //            ]), startPoint: .top, endPoint: .bottom)
                 
-//                LinearGradient(gradient: Gradient(stops: [
-//                        Gradient.Stop(color: .white, location: 0.45),
-//                        Gradient.Stop(color: .black, location: 0.55),
-//                    ]), startPoint: .top, endPoint: .bottom)
+                //                LinearGradient(gradient: Gradient(stops: [
+                //                        Gradient.Stop(color: .white, location: 0.45),
+                //                        Gradient.Stop(color: .black, location: 0.55),
+                //                    ]), startPoint: .top, endPoint: .bottom)
                 
                 LinearGradient(gradient: Gradient(colors: [.pink, .black]), startPoint: .top, endPoint: .bottom)
                 
             }
             //Color(red: 0.6, green: 0.9, blue: 1)
-                //.ignoresSafeArea()
-                //.frame(width: 400, height: 400)
-                //.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+            //.ignoresSafeArea()
+            //.frame(width: 400, height: 400)
+            //.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+            
+            VStack {
                 Text("What a day!")
                     .padding(60)
-                    //.foregroundColor(.secondary)
+                //.foregroundColor(.secondary)
                     .foregroundStyle(.secondary) //vibrancy
                     .background(.ultraThinMaterial) //frosted glass material
-            
-            
-            
+                Button("Find out more", role: .destructive, action: executeData)
+                Button("Button 1") { }
+                        .buttonStyle(.bordered)
+                        .tint(.mint)
+                Button("Button 2", role: .destructive) { }
+                        .buttonStyle(.bordered)
+                Button("Button 3") { }
+                        .buttonStyle(.borderedProminent)
+                        .tint(.mint)
+                Button("Button 4", role: .destructive) { }
+                        .buttonStyle(.borderedProminent)
+                
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Label("Edit", systemImage: "pencil")
+                }
+                
+                Button {
+                    print("Button was tapped")
+                } label: {
+                    Text("Tap me!")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.red)
+                        .cornerRadius(10)
+                }
+                
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Image(systemName: "pencil")
+                        .scaleEffect(3)
+                }
+                
+            }
             
         }
         //background for text
@@ -58,6 +98,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-.previewInterfaceOrientation(.portrait)
+            .previewInterfaceOrientation(.portrait)
     }
 }
