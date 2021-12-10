53917ce72ba5fd0282d5cb0abacb89087a86dc2c
citixenken <kmuyesu@gmail.com>
2021-12-10 23:42:21 +0300

    stacking up buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,102 +8,127 @@
 import SwiftUI
 
 struct ContentView: View {
+    //
+    //    func executeData() {
+    //        print("Here we go...")
+    //    }
+    //
+    //    @State private var showingAlert = true
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "US", "UK"]
     
-    func executeData() {
-        print("Here we go...")
-    }
-    
-    @State private var showingAlert = true
+    var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        
         ZStack {
-            //entire screen background
-            VStack (spacing: 0){
-                //Color.red
-                //Color.blue
-                
-                //gradients
-                
-                //                AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
-                
-                //                RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
-                
-                //            LinearGradient(gradient: Gradient(stops: [
-                //                    .init(color: .white, location: 0.30),
-                //                    .init(color: .black, location: 0.70),
-                //            ]), startPoint: .top, endPoint: .bottom)
-                
-                //                LinearGradient(gradient: Gradient(stops: [
-                //                        Gradient.Stop(color: .white, location: 0.45),
-                //                        Gradient.Stop(color: .black, location: 0.55),
-                //                    ]), startPoint: .top, endPoint: .bottom)
-                
-                LinearGradient(gradient: Gradient(colors: [.pink, .black]), startPoint: .top, endPoint: .bottom)
-                
-            }
-            //Color(red: 0.6, green: 0.9, blue: 1)
-            //.ignoresSafeArea()
-            //.frame(width: 400, height: 400)
-            //.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+            Color.blue
+                .ignoresSafeArea()
             
-            VStack {
-                Text("What a day!")
-                    .padding(60)
-                //.foregroundColor(.secondary)
-                    .foregroundStyle(.secondary) //vibrancy
-                    .background(.ultraThinMaterial) //frosted glass material
-                Button("Find out more", role: .destructive, action: executeData)
-                Button("Button 1") { }
-                        .buttonStyle(.bordered)
-                        .tint(.mint)
-                Button("Button 2", role: .destructive) { }
-                        .buttonStyle(.bordered)
-                Button("Button 3") { }
-                        .buttonStyle(.borderedProminent)
-                        .tint(.mint)
-                Button("Button 4", role: .destructive) { }
-                        .buttonStyle(.borderedProminent)
-                
-                Button {
-                    print("Edit button was tapped")
-                } label: {
-                    Label("Edit", systemImage: "pencil")
-                }
-                
-                Button {
-                    print("Button was tapped")
-                } label: {
-                    Text("Tap me!")
-                        .padding()
+            VStack (spacing: 30){
+                VStack {
+                    Text("Tap the flag of...")
                         .foregroundColor(.white)
-                        .background(.red)
-                        .cornerRadius(10)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
                 }
-                
-                Button {
-                    print("Edit button was tapped")
-                } label: {
-                    Image(systemName: "pencil")
-                        .scaleEffect(3)
-                }
-                
-                Button("Show Alert") {
-                    showingAlert = false
-                }
-                .alert("Important message", isPresented: $showingAlert) {
-                    Button("Delete", role: .destructive) {}
-                    Button("Cancel", role: .cancel) {}
-                } message: {
-                    Text("Please read this first.")
+                ForEach (0..<3) { number in
+                    Button {
+                        //flag was tapped
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original) //render original pixels rather than recolouring them as a button
+                    }
                 }
                 
             }
-            
         }
-        //background for text
-        .background(.blue)
-        .ignoresSafeArea()
+        //
+        //        ZStack {
+        //            //entire screen background
+        //            VStack (spacing: 0){
+        //                //Color.red
+        //                //Color.blue
+        //
+        //                //gradients
+        //
+        //                //                AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+        //
+        //                //                RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+        //
+        //                //            LinearGradient(gradient: Gradient(stops: [
+        //                //                    .init(color: .white, location: 0.30),
+        //                //                    .init(color: .black, location: 0.70),
+        //                //            ]), startPoint: .top, endPoint: .bottom)
+        //
+        //                //                LinearGradient(gradient: Gradient(stops: [
+        //                //                        Gradient.Stop(color: .white, location: 0.45),
+        //                //                        Gradient.Stop(color: .black, location: 0.55),
+        //                //                    ]), startPoint: .top, endPoint: .bottom)
+        //
+        //                LinearGradient(gradient: Gradient(colors: [.pink, .black]), startPoint: .top, endPoint: .bottom)
+        //
+        //            }
+        //            //Color(red: 0.6, green: 0.9, blue: 1)
+        //            //.ignoresSafeArea()
+        //            //.frame(width: 400, height: 400)
+        //            //.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+        //
+        //            VStack {
+        //                Text("What a day!")
+        //                    .padding(60)
+        //                //.foregroundColor(.secondary)
+        //                    .foregroundStyle(.secondary) //vibrancy
+        //                    .background(.ultraThinMaterial) //frosted glass material
+        //                Button("Find out more", role: .destructive, action: executeData)
+        //                Button("Button 1") { }
+        //                        .buttonStyle(.bordered)
+        //                        .tint(.mint)
+        //                Button("Button 2", role: .destructive) { }
+        //                        .buttonStyle(.bordered)
+        //                Button("Button 3") { }
+        //                        .buttonStyle(.borderedProminent)
+        //                        .tint(.mint)
+        //                Button("Button 4", role: .destructive) { }
+        //                        .buttonStyle(.borderedProminent)
+        //
+        //                Button {
+        //                    print("Edit button was tapped")
+        //                } label: {
+        //                    Label("Edit", systemImage: "pencil")
+        //                }
+        //
+        //                Button {
+        //                    print("Button was tapped")
+        //                } label: {
+        //                    Text("Tap me!")
+        //                        .padding()
+        //                        .foregroundColor(.white)
+        //                        .background(.red)
+        //                        .cornerRadius(10)
+        //                }
+        //
+        //                Button {
+        //                    print("Edit button was tapped")
+        //                } label: {
+        //                    Image(systemName: "pencil")
+        //                        .scaleEffect(3)
+        //                }
+        //
+        //                Button("Show Alert") {
+        //                    showingAlert = false
+        //                }
+        //                .alert("Important message", isPresented: $showingAlert) {
+        //                    Button("Delete", role: .destructive) {}
+        //                    Button("Cancel", role: .cancel) {}
+        //                } message: {
+        //                    Text("Please read this first.")
+        //                }
+        //
+        //            }
+        //
+        //        }
+        //        //background for text
+        //        .background(.blue)
+        //        .ignoresSafeArea()
     }
 }
 
